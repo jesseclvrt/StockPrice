b5e38a61da46c91f4c3c24516be69690f834b1c8
Jesse Calvert <jesseclvrt1@gmail.com>
2018-11-06 09:29:17 -0800

    better comments

--- StockPrice.java
+++ StockPrice.java
@@ -3,30 +3,23 @@
  * 11/17/2017
  */
 
-package stockprice;
-
 import java.net.URL;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 
-/**
- * Prints the price of the given stock from the command-line.
- * 
- * Sample call (cmd):
- * >java -Xmx1024m -jar StockPrice.jar FB
- * >179.00
- */
+
+// Prints the price of the given stock from the command-line.
 public class StockPrice {
 
     /**
-     * Checks for exactly one command-line argument and finds the closing price, else prints an error message.
-     * 
-     * @param args the command line arguments
+     * Finds closing price of exactly one argument,
+     * else prints an error message.
      */
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.out.println("Invalid call. Please use exactly one stock symbol as an argument.");
+            System.out.println("Invalid call. Please use exactly "
+                + "one stock symbol as an argument.");
         } else {
             try {
                 new StockPrice().getClosingPrice(args[0]);
@@ -40,7 +33,7 @@ public class StockPrice {
      * Finds the closing price of a given stock symbol from Quandl's database.
      * 
      * @param stockName the stock symbol to search for
-     * @throws Exception if there is a problem connecting. User may have to set the proxy host to connect
+     * @throws Exception if there is a problem connecting
      */
     private void getClosingPrice(String stockName) throws Exception {
         final int closingLoc = 4;                       //element the closing price occurs.
@@ -49,9 +42,9 @@ public class StockPrice {
         URL quandl = new URL(url);
         BufferedReader in = new BufferedReader(
             new InputStreamReader(quandl.openStream()));
-        in.readLine();                                  //gets rid of data header
-        String dataLine = in.readLine();                //get the actual data (csv format)
-        in.close();                                     //close stream
+        in.readLine();                                  //skip data header
+        String dataLine = in.readLine();                //actual data
+        in.close();
         String[] dataArray = dataLine.split(",");
         System.out.println(new BigDecimal(dataArray[closingLoc]));
     }
